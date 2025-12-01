618bb69701fbd0ece30466e00999f1c3bbef5f1e
Resolute Education <99249773+resolute-support@users.noreply.github.com>
2025-12-01 13:35:49 +0200

    Moved blocks to advanced category

--- oled.ts
+++ oled.ts
@@ -180,6 +180,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_PIXEL" block="set pixel at x %x|y %y|color %color"
     //% weight=70 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function pixel(x: number, y: number, color: number = 1) {
         let page = y >> 3
         let shift_page = y % 8
@@ -261,6 +262,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_HLINE" block="draw a horizontal line at x %x|y %y|number %len|color %color"
     //% weight=71 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function hline(x: number, y: number, len: number, color: number = 1) {
         for (let i = x; i < (x + len); i++)
             pixel(i, y, color)
@@ -276,6 +278,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_VLINE" block="draw a vertical line at x %x|y %y|number %len|color %color"
     //% weight=72 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function vline(x: number, y: number, len: number, color: number = 1) {
         for (let i = y; i < (y + len); i++)
             pixel(x, i, color)
@@ -292,6 +295,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_RECT" block="draw a rectangle at x1 %x1|y1 %y1|x2 %x2|y2 %y2|color %color"
     //% weight=73 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function rect(x1: number, y1: number, x2: number, y2: number, color: number = 1) {
         if (x1 > x2)
             x1 = [x2, x2 = x1][0];
@@ -310,6 +314,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_INVERT" block="invert display %d"
     //% weight=65 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function invert(d: boolean = true) {
         let n = (d) ? 0xA7 : 0xA6
         cmd1(n)
@@ -365,6 +370,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_ZOOM" block="zoom %d"
     //% weight=60 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function zoom(d: boolean = true) {
         _ZOOM = (d) ? 1 : 0
         cmd2(0xd6, _ZOOM)
@@ -377,6 +383,7 @@ namespace OLED {
     //% blockId="OLED12864_I2C_init" block="init OLED with addr %addr"
     //% weight=100 blockGap=8
     //% parts=OLED12864_I2C trackArgs=0
+    //% advanced=true
     export function init(addr: number) {
         _I2CAddr = addr;
         cmd1(0xAE)       // SSD1306_DISPLAYOFF

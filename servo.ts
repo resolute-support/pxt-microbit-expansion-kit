acf2e5d22971e14f264de6c6f8185f0cdd50e89d
Resolute Education <99249773+resolute-support@users.noreply.github.com>
2025-12-09 14:21:19 +0200

    Update servo.ts

--- servo.ts
+++ servo.ts
@@ -8,11 +8,9 @@ namespace servo360 {
         AntiClockwise = 1
     }
 
-    //% block="servo pin %pin rotate %direction at %power \\%"
+    //% block="servo pin %pin rotate %direction at %power \\%" 
     //% pin.shadow="pins.analogPin"
-    //% power.shadow="number"
-    //% power.min=0 power.max=100
-    //% power.defl=50
+    //% power.shadow="number" power.defl=50 power.min=0 power.max=100
     export function servoWritePin(pin: AnalogPin, direction: Direction, power: number): void {
         let pulse = 1500
         if (direction === Direction.Clockwise) {

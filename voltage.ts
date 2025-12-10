2d52d5e662434527ad65de30b85df48502680daa
Resolute Education <99249773+resolute-support@users.noreply.github.com>
2025-12-10 17:08:43 +0200

    implemented voltage sensor

---
+++ voltage.ts
@@ -0,0 +1,26 @@
+//% color="#ff6800" icon="\uf0e7" weight=15 block="Voltage Sensor"
+namespace VoltageSensor {
+
+    let sensorPin: AnalogPin = undefined
+
+    /**
+     * Attach Voltage sensor to a pin
+     */
+    //% block="attach Voltage sensor to pin %pin"
+    //% pin.shadow="pins.analogPin"
+    //% weight=100
+    export function attachSensor(pin: AnalogPin): void {
+        sensorPin = pin
+    }
+
+    /**
+     * Read voltage from sensor
+     */
+    //% block="read Voltage temperature"
+    export function readVoltage(): number {
+        if (sensorPin === undefined) return 0
+        let reading = pins.analogReadPin(AnalogPin.P0)
+        let voltage = reading * (16.5/1023)
+        return voltage
+    }
+}
